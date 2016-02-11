2d37db052e5f74f03c0c22add8d411be9422fe14
dennisngeno <dennisngeno7@gmail.com>
2016-02-11 19:53:05 +0300

    Crated a tag

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.views import  generic
 
-# Create your views here.
+domain = 'http://127.0.0.1:8000'
+
+
+class DashBoard(LoginRequiredMixin, generic.DetailView ):
+    model = User
+    template_name = 'accounts/dash_board.html'
+    select_related = ('endpoints',)
+
+    def get_object(self, queryset = None):
+        return self.request.user
