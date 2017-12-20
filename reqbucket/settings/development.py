c3bf53eff8b03163106a043795a92b68bd650213
dennisngeno <dennisngeno7@gmail.com>
2017-12-20 21:46:35 +0300

    worked on setting and database connection due to dajngo2

---
+++ reqbucket/settings/development.py
@@ -0,0 +1,15 @@
+from . base import *
+DEBUG = True
+
+# MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'putsreq', #put the database name
+        'USER': 'adoniswalker', #put your username
+        'PASSWORD': 'adonis254', #put your password
+        'HOST': 'localhost', #specify the host
+        'PORT': '5432', #specify the port
+    }
+}
+
